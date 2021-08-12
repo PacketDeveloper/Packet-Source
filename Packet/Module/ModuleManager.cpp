cfa3aa6593be25419ff2f40089729796db6f7641
Little Kow <theterrarian101@gmail.com>
2021-08-12 14:55:36 -0400

    Added Spider & PacketMuliplier

--- Packet/Module/ModuleManager.cpp
+++ Packet/Module/ModuleManager.cpp
@@ -75,6 +75,7 @@ void ModuleManager::initModules() {
 		moduleList.push_back(std::shared_ptr<IModule>(new Spammer()));
 		moduleList.push_back(std::shared_ptr<IModule>(new CrystalAura()));
 		moduleList.push_back(std::shared_ptr<IModule>(new BehindAura()));
+		moduleList.push_back(std::shared_ptr<IModule>(new Spider()));
 
 		// Unused modules
 		//moduleList.push_back(std::shared_ptr<IModule>(new Derp()));
@@ -96,6 +97,7 @@ void ModuleManager::initModules() {
 		moduleList.push_back(std::shared_ptr<IModule>(new TestModule()));
 		moduleList.push_back(std::shared_ptr<IModule>(new HiveFly()));
 		moduleList.push_back(std::shared_ptr<IModule>(new Survival()));
+		moduleList.push_back(std::shared_ptr<IModule>(new Emote()));
 		// Broken
 		moduleList.push_back(std::shared_ptr<IModule>(new TriggerBot()));
 		//moduleList.push_back(std::shared_ptr<IModule>(new NoSlow()));
