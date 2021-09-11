9d177ae3d0796226c326e819c254ef98fd791a4b
deq <86852233+deqqq@users.noreply.github.com>
2021-09-11 08:49:59 -0400

    started hud will add armorhud

--- Memory/Hooks.cpp
+++ Memory/Hooks.cpp
@@ -1240,34 +1240,79 @@ __int64 Hooks::RenderText(__int64 a1, C_MinecraftUIRenderContext* renderCtx) {
 	HImGui.endFrame();
 	DrawUtils::flush();
 
-	// Draw FPS
+		// Draw FPS
 	if (hudMod->isEnabled() && hudMod->fps && g_Data.canUseMoveKeys()) {
 		vec2_t windowSize = g_Data.getClientInstance()->getGuiData()->windowSize;
 		std::string fpsText = "FPS: " + std::to_string(g_Data.getFPS());
 
-		float x = windowSize.x / 114 + 1;
-		float y = windowSize.y - 10;
+		float startY = windowSize.y - 35;
+		float l = DrawUtils::getTextWidth(&fpsText, 1) + 6.5;
+
+		vec4_t rectPos = vec4_t(2.5, startY + 6, l, startY + 20);
+		vec2_t textPos = vec2_t(rectPos.x + 2, rectPos.y + 3);
+
+		DrawUtils::fillRectangle(rectPos, MC_Color(0, 0, 0), hudMod->opacity);
 
 		if (hudMod->color.getSelectedValue() != 1)  // Rainbowf
-			DrawUtils::drawText(vec2_t(x, y), &fpsText, MC_Color(currColor), 1, 1, true);
+			DrawUtils::drawText(textPos, &fpsText, MC_Color(currColor), 1, 1, true);
 		if (hudMod->color.getSelectedValue() == 1) {  // Dynamic
-			DrawUtils::drawText(vec2_t(x, y), &fpsText, MC_Color(dynamic, dynamic, dynamic), 1, 1, true);
+			DrawUtils::drawText(textPos, &fpsText, MC_Color(dynamic, dynamic, dynamic), 1, 1, true);
 		} else if (hudMod->color.getSelectedValue() == 4) {  // White
-			DrawUtils::drawText(vec2_t(x, y), &fpsText, MC_Color(255, 255, 255), 1, 1, true);
+			DrawUtils::drawText(textPos, &fpsText, MC_Color(255, 255, 255), 1, 1, true);
 		} else if (hudMod->color.getSelectedValue() == 5) {  // Red
-			DrawUtils::drawText(vec2_t(x, y), &fpsText, MC_Color(255, 0, 0), 1, 1, true);
+			DrawUtils::drawText(textPos, &fpsText, MC_Color(255, 0, 0), 1, 1, true);
 		} else if (hudMod->color.getSelectedValue() == 6) {  // Orange
-			DrawUtils::drawText(vec2_t(x, y), &fpsText, MC_Color(255, 127, 0), 1, 1, true);
+			DrawUtils::drawText(textPos, &fpsText, MC_Color(255, 127, 0), 1, 1, true);
 		} else if (hudMod->color.getSelectedValue() == 7) {  // Yellow
-			DrawUtils::drawText(vec2_t(x, y), &fpsText, MC_Color(255, 255, 0), 1, 1, true);
+			DrawUtils::drawText(textPos, &fpsText, MC_Color(255, 255, 0), 1, 1, true);
 		} else if (hudMod->color.getSelectedValue() == 8) {  // Green
-			DrawUtils::drawText(vec2_t(x, y), &fpsText, MC_Color(0, 255, 0), 1, 1, true);
+			DrawUtils::drawText(textPos, &fpsText, MC_Color(0, 255, 0), 1, 1, true);
 		} else if (hudMod->color.getSelectedValue() == 9) {  // Blue
-			DrawUtils::drawText(vec2_t(x, y), &fpsText, MC_Color(0, 170, 255), 1, 1, true);
+			DrawUtils::drawText(textPos, &fpsText, MC_Color(0, 170, 255), 1, 1, true);
 		} else if (hudMod->color.getSelectedValue() == 11) {  // Purple
-			DrawUtils::drawText(vec2_t(x, y), &fpsText, MC_Color(148, 0, 211), 1, 1, true);
+			DrawUtils::drawText(textPos, &fpsText, MC_Color(148, 0, 211), 1, 1, true);
 		} else if (hudMod->color.getSelectedValue() == 12) {  // Pink
-			DrawUtils::drawText(vec2_t(x, y), &fpsText, MC_Color(255, 192, 203), 1, 1, true);
+			DrawUtils::drawText(textPos, &fpsText, MC_Color(255, 192, 203), 1, 1, true);
+		}
+	}
+
+	// pos
+	{
+		if (hudMod->coordinates && !clickGuiModule->isEnabled() && g_Data.canUseMoveKeys()) {
+			vec2_t windowSize = g_Data.getClientInstance()->getGuiData()->windowSize;
+			vec3_t* pos = g_Data.getLocalPlayer()->getPos();
+
+			std::string position = "Position: " + std::to_string((int)floorf(pos->x)) + " " + std::to_string((int)floorf(pos->y)) + " " + std::to_string((int)floorf(pos->z));
+
+			float startY =  windowSize.y - 21;
+			float l = DrawUtils::getTextWidth(&position, 1) + 6.5;
+
+			vec4_t rectPos = vec4_t(2.5, startY + 6, l, startY + 20);
+			vec2_t textPos = vec2_t(rectPos.x + 2, rectPos.y + 3);
+			
+			DrawUtils::fillRectangle(rectPos, MC_Color(0, 0, 0), hudMod->opacity);
+
+			if (hudMod->color.getSelectedValue() != 1)  // Rainbowf
+				DrawUtils::drawText(textPos, &position, MC_Color(currColor), 1, 1, true);
+			if (hudMod->color.getSelectedValue() == 1) {  // Dynamic
+				DrawUtils::drawText(textPos, &position, MC_Color(dynamic, dynamic, dynamic), 1, 1, true);
+			} else if (hudMod->color.getSelectedValue() == 4) {  // White
+				DrawUtils::drawText(textPos, &position, MC_Color(255, 255, 255), 1, 1, true);
+			} else if (hudMod->color.getSelectedValue() == 5) {  // Red
+				DrawUtils::drawText(textPos, &position, MC_Color(255, 0, 0), 1, 1, true);
+			} else if (hudMod->color.getSelectedValue() == 6) {  // Orange
+				DrawUtils::drawText(textPos, &position, MC_Color(255, 127, 0), 1, 1, true);
+			} else if (hudMod->color.getSelectedValue() == 7) {  // Yellow
+				DrawUtils::drawText(textPos, &position, MC_Color(255, 255, 0), 1, 1, true);
+			} else if (hudMod->color.getSelectedValue() == 8) {  // Green
+				DrawUtils::drawText(textPos, &position, MC_Color(0, 255, 0), 1, 1, true);
+			} else if (hudMod->color.getSelectedValue() == 9) {  // Blue
+				DrawUtils::drawText(textPos, &position, MC_Color(0, 170, 255), 1, 1, true);
+			} else if (hudMod->color.getSelectedValue() == 11) {  // Purple
+				DrawUtils::drawText(textPos, &position, MC_Color(148, 0, 211), 1, 1, true);
+			} else if (hudMod->color.getSelectedValue() == 12) {  // Pink
+				DrawUtils::drawText(textPos, &position, MC_Color(255, 192, 203), 1, 1, true);
+			}
 		}
 	}
 
