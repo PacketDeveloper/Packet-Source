c204acb1c6fce66077c9089d576d409c9688efe0
deq <86852233+deqqq@users.noreply.github.com>
2021-09-16 07:59:05 -0400

    good instabreak

--- Packet/Module/Modules/InstaBreak.cpp
+++ Packet/Module/Modules/InstaBreak.cpp
@@ -1,6 +1,7 @@
 #include "InstaBreak.h"
 
 InstaBreak::InstaBreak() : IModule(0, Category::EXPLOIT, "Break any block instantly") {
+	registerBoolSetting("Hold", &breakBlocks, breakBlocks);
 }
 
 InstaBreak::~InstaBreak() {
@@ -8,4 +9,16 @@ InstaBreak::~InstaBreak() {
 
 const char* InstaBreak::getModuleName() {
 	return ("InstaBreak");
+}
+
+void InstaBreak::onTick(C_GameMode* gm) {
+	bool hold = true;
+	if ((GameData::isLeftClickDown() || !hold) && GameData::canUseMoveKeys()) {
+
+		PointingStruct* pointing = g_Data.getClientInstance()->getPointerStruct();
+
+		if (breakBlocks) {
+			gm->destroyBlock(&pointing->block, pointing->blockSide);
+		}
+	}
 }
--- Packet/Module/Modules/InstaBreak.h
+++ Packet/Module/Modules/InstaBreak.h
@@ -3,8 +3,14 @@
 
 class InstaBreak : public IModule {
 public:
+	bool breakBlocks = false;
+	int delay = 0;
+	int Odelay = 0;
+
 	InstaBreak();
 	~InstaBreak();
+
 	// Inherited via IModule
+	virtual void onTick(C_GameMode* gm) override;
 	virtual const char* getModuleName() override;
 };
