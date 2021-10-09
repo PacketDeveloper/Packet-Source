09c2815624d851a6a62ceb0f44c1e00c2336fbe8
Packet <86270028+PacketDeveloper@users.noreply.github.com>
2021-10-09 07:00:07 -0700

    thx github

--- Packet/Module/Modules/InstaBreak.cpp
+++ Packet/Module/Modules/InstaBreak.cpp
@@ -13,7 +13,6 @@ const char* InstaBreak::getModuleName() {
 
 void InstaBreak::onTick(C_GameMode* gm) {
 	bool hold = true;
-<<<<<<< HEAD
 	if (g_Data.canUseMoveKeys() && breakBlocks) {
 		if (hold && !g_Data.isLeftClickDown())
 			return;
@@ -21,14 +20,3 @@ void InstaBreak::onTick(C_GameMode* gm) {
 		gm->destroyBlock(&pointing->block, pointing->blockSide);
 	}
 }
-=======
-	if ((GameData::isLeftClickDown() || !hold) && GameData::canUseMoveKeys()) {
-
-		PointingStruct* pointing = g_Data.getClientInstance()->getPointerStruct();
-
-		if (breakBlocks) {
-			gm->destroyBlock(&pointing->block, pointing->blockSide);
-		}
-	}
-}
->>>>>>> 2d6938d96a0dbefd445141e7f1edfc47b99dfcf0
